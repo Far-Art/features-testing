e63a1ae46c411b89d9841e7ccc948aa829f568d0
far-art <arturfarmanov91@gmail.com>
2026-03-14 12:17:48 +0200

    changes to env

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,21 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {ScoopedCircleComponent} from './scooped-circle/scooped-circle';
+import {Component, signal} from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {ImsGrid} from './components/ims-grid/ims-grid';
+import {ImsGridRow} from './components/ims-grid/ims-grid-row';
+import {ImsGridCell} from './components/ims-grid/ims-grid-cell';
+
 
 @Component({
-  selector: 'app-root',
-    imports: [RouterOutlet, ScoopedCircleComponent],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [
+        MatExpansionModule,
+        ImsGrid,
+        ImsGridRow,
+        ImsGridCell
+    ],
+    templateUrl: './app.html',
+    styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('features-testing');
+    protected readonly title = signal('features-testing');
 }
