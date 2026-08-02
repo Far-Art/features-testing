b535b327eb838cc157944e7de49b9114ae8dcc1d
far-art <arturfarmanov91@gmail.com>
2026-08-02 11:19:43 +0300

    explicitly export api

--- src/app/components/ims-dialog/index.ts
+++ src/app/components/ims-dialog/index.ts
@@ -1,6 +1,21 @@
-export * from './ims-abstract-dialog';
-export * from './ims-dialog-builder';
-export * from './ims-dialog-ref';
-export * from './ims-dialog-section';
-export * from './ims-dialog.service';
-export * from './ims-dialog.types';
+export { ImsAbstractDialog } from './ims-abstract-dialog';
+export { ImsDialogBuilder } from './ims-dialog-builder';
+export type { ImsDialogBuilderHost } from './ims-dialog-builder';
+export { ImsDialogRef } from './ims-dialog-ref';
+export {
+  ImsDialogActions,
+  ImsDialogContent,
+  ImsDialogTitle,
+  ImsDialogToolbar,
+} from './ims-dialog-section';
+export { ImsDialogService } from './ims-dialog.service';
+export { IMS_DIALOG_CONFIG, IMS_DIALOG_DATA, resolveConfirmationLabels } from './ims-dialog.types';
+export type {
+  ImsDialogConfirmationLabels,
+  ImsDialogContentType,
+  ImsDialogMode,
+  ImsDialogOpenOptions,
+  ImsDialogResolvedConfirmationLabels,
+  ImsDialogRuntimeConfig,
+  ImsDialogSeverity,
+} from './ims-dialog.types';
