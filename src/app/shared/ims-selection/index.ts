f200b2e8a53bae8dcdf1684a2477e420c346f105
far-art <arturfarmanov91@gmail.com>
2026-09-17 11:08:13 +0300

    explicitly export types

--- src/app/shared/ims-selection/index.ts
+++ src/app/shared/ims-selection/index.ts
@@ -1,3 +1,17 @@
-export * from './ims-selection.types';
-export * from './ims-selection-readonly-panel';
-export * from './ims-selection-toolbar';
+export { ImsSelectionReadonlyPanel } from './ims-selection-readonly-panel';
+export { ImsSelectionToolbar } from './ims-selection-toolbar';
+export {
+  IMS_SELECTION_DEFAULT_LABELS,
+  IMS_SELECTION_EMPTY_DISPLAY,
+  IMS_SELECTION_LABELS,
+} from './ims-selection.types';
+export type {
+  ImsSelectionAutoMode,
+  ImsSelectionCompareWith,
+  ImsSelectionDisplayState,
+  ImsSelectionEditDialogMode,
+  ImsSelectionLabels,
+  ImsSelectionOverlaySide,
+  ImsSelectionToolbarSide,
+  ImsSelectionViewMode,
+} from './ims-selection.types';
